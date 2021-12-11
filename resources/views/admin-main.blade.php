96c1e7a77c7320be3aaee80aef803253e1f9eab0
Sadam Alfian <sadamalfianp@gmail.com>
2021-12-11 10:13:49 +0700

    add logout button on admin

--- resources/views/admin-main.blade.php
+++ resources/views/admin-main.blade.php
@@ -7,27 +7,14 @@
 @section('content')
   <header class="w-full bg-bg_large relative">
     <nav class="header max-w-mobile mx-auto flex justify-between items-center">
-      <a href="/">
-        <svg width="32" height="30" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M46.6933 10.3201L41.9466 5.6001L15.5733 32.0001L41.9733 58.4001L46.6933 53.6801L25.0133 32.0001L46.6933 10.3201Z"
-            fill="#fff"/>
-        </svg>
-      </a>
-       <h1 class="text-lg -mr-6">Dashboard Admin</h1>
-
-       <div id="dropdown" class="text-center relative">
-         <button onclick="toggleLogout()" id="logout" class="cursor-pointer">
+       <h1 class="text-lg -mr-6">Admin</h1>
+        <div class="flex">
           <img class="mx-auto" src="{{ asset('/src/icons/Profile.svg') }}" alt="logout">
-          <h3>Fulanah</h3>
-         </button>
-         <div id="dropdown-content" class="absolute hidden bg-gray-200 overflow-auto w-32 z-10 top-20 -right-6">
-          <a class="block p-3 mx-auto no-underline text-black" href="/">
-            <img class="w-5 h-5 inline" src="{{ asset('/src/icons/Logout.svg') }}" alt="Logout">
-            Logout
-          </a>
-         </div>
-       </div>
+          <div class="ml-1">
+            <h3 class="text-sm">Fulanah</h3>
+            <h4 class="text-xs">Admin</h4>
+          </div>
+        </div>
     </nav>
   </header>
 
@@ -45,6 +32,19 @@
           Tambah Perusahaan
         </a>
       </div>
+      {{-- Layout Super Admin --}}
+      {{-- <div class="box rounded-lg flex-col p-2">
+        <img class="w-12 h-12 block mb-3" src="{{ asset('/src/icons/clipboard.svg') }}" alt="Add Review">
+        <a class="font-medium text-xs text-white text-center">
+          Tambah Ulasan
+        </a>
+      </div>
+      <div class="box rounded-lg flex-col p-2">
+        <img class="w-12 h-12 block mb-3" src="{{ asset('/src/icons/user.svg') }}" alt="User logo">
+        <a class="font-medium text-xs text-white text-center">
+          Tambah Admin
+        </a>
+      </div> --}}
     </div>
     <div class="box rounded-lg flex-col p-2 mt-10 mx-auto">
       <img class="w-12 h-12 block mb-3" src="{{ asset('/src/icons/clipboard.svg') }}" alt="Add Review">
@@ -52,6 +52,11 @@
         Tambah Ulasan
       </a>
     </div>
+    <button 
+    class="p-2 w-full bg-lavender_rose text-white rounded-xl text-center cursor-pointer hover:shadow-md mt-32" 
+    type="submit" name="submit">
+      Logout
+    </button>
   </div>
 
   @section('scripts')
