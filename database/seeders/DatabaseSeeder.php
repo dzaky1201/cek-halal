031f93726c3b3de91f9f7283658b296510f77b0a
dzaky1201 <dzakyhdr@gmail.com>
2021-12-23 08:37:56 +0700

    fix bug 1.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,61 +28,61 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345678')
         ]);
 
-        User::create([
-            'name' => 'abdullah',
-            'username' => 'abdullah',
-            'email' => 'abdullah@google.com',
-            'role' => 'admin',
-            'password' => bcrypt('12345678')
-        ]);
+        // User::create([
+        //     'name' => 'abdullah',
+        //     'username' => 'abdullah',
+        //     'email' => 'abdullah@google.com',
+        //     'role' => 'admin',
+        //     'password' => bcrypt('12345678')
+        // ]);
 
 
-        Company::create([
-            'name' => 'PT.Indofood',
-            'url_website' => 'www.indofood.com',
-        ]);
+        // Company::create([
+        //     'name' => 'PT.Indofood',
+        //     'url_website' => 'www.indofood.com',
+        // ]);
 
-        Company::create([
-            'name' => 'PT.Unilever',
-            'url_website' => 'www.unilever.com',
-        ]);
+        // Company::create([
+        //     'name' => 'PT.Unilever',
+        //     'url_website' => 'www.unilever.com',
+        // ]);
 
-        Product::create([
-            'company_id' => 1,
-            'name' => 'Indomie Goreng',
-            'user_id' => 2,
-            'is_halal' => 'true',
-            'ingredients' => 'tepung, minyak, bawang, dll',
-            'expire_date' => '12-01-2001',
-            'certification_number' => 'xxxx',
-            'image' => 'default.jpg',
-        ]);
+        // Product::create([
+        //     'company_id' => 1,
+        //     'name' => 'Indomie Goreng',
+        //     'user_id' => 2,
+        //     'is_halal' => 'true',
+        //     'ingredients' => 'tepung, minyak, bawang, dll',
+        //     'expire_date' => '12-01-2001',
+        //     'certification_number' => 'xxxx',
+        //     'image' => 'default.jpg',
+        // ]);
 
-        Product::create([
-            'company_id' => 1,
-            'name' => 'Indomie Goreng Rendang',
-            'user_id' => 1,
-            'is_halal' => 'true',
-            'ingredients' => 'tepung, minyak, bawang, dll',
-            'expire_date' => '12-01-2001',
-            'certification_number' => 'xxxx',
-            'image' => 'default1.jpg',
-        ]);
+        // Product::create([
+        //     'company_id' => 1,
+        //     'name' => 'Indomie Goreng Rendang',
+        //     'user_id' => 1,
+        //     'is_halal' => 'true',
+        //     'ingredients' => 'tepung, minyak, bawang, dll',
+        //     'expire_date' => '12-01-2001',
+        //     'certification_number' => 'xxxx',
+        //     'image' => 'default1.jpg',
+        // ]);
 
-        Review::create([
-            'user_id' => 1,
-            'product_id' => 1,
-            'content' => 'ini adalah makanan halal guys',
-            'url_source' => 'www.google.com',
-            'image' => 'default.jpg'
-        ]);
+        // Review::create([
+        //     'user_id' => 1,
+        //     'product_id' => 1,
+        //     'content' => 'ini adalah makanan halal guys',
+        //     'url_source' => 'www.google.com',
+        //     'image' => 'default.jpg'
+        // ]);
 
-        Review::create([
-            'user_id' => 1,
-            'product_id' => 2,
-            'content' => 'ini adalah makanan halal guys',
-            'url_source' => 'www.google.com',
-            'image' => 'default.jpg'
-        ]);
+        // Review::create([
+        //     'user_id' => 1,
+        //     'product_id' => 2,
+        //     'content' => 'ini adalah makanan halal guys',
+        //     'url_source' => 'www.google.com',
+        //     'image' => 'default.jpg'
+        // ]);
     }
 }
